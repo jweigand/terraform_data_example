796a2bbe407f130d18a8e2bfab2e11d4236f1038
John Weigand <john.weigand@hashicorp.com>
2024-10-23 13:58:05 -0400

    multiple resources

--- main.tf
+++ main.tf
@@ -21,14 +21,30 @@ variable "triggers_replace" {
   default = "demo"
 }
 
-resource "terraform_data" "bootstrap" {
+resource "terraform_data" "var_based" {
   triggers_replace = var.triggers_replace
 
   provisioner "local-exec" {
-    command = "echo this only happens on first create or when the triggers change"
+    command = "echo this only happens on first create or when the variable changes"
   }
 }
 
 resource "random_pet" "this" {
   length = 3
+}
+
+resource "terraform_data" "res_based" {
+  triggers_replace = random_pet.this.id
+
+  provisioner "local-exec" {
+    command = "echo this only happens on first create or when the resource changes"
+  }
+}
+
+resource "terraform_data" "string_based" {
+  triggers_replace = "test"
+
+  provisioner "local-exec" {
+    command = "echo this only happens on first create or when the string changes"
+  }
 }
