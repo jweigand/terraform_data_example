49ca06bc240da8f9ccbeb104d658b22d92f16641
John Weigand <john.weigand@hashicorp.com>
2024-10-23 12:37:35 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.8"
+
+  cloud {
+    organization = "weigand-hcp"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "tf_data_ephemeral_resource"
+    }
+  }
+
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+
+resource "terraform_data" "bootstrap" {
+  triggers_replace = {
+    value = "demo"
+  }
+
+  provisioner "local-exec" {
+    command = "echo this only happens on first create or when the triggers change"
+  }
+}
